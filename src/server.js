fc506837b5adf629d845dceb304d1d3ffb9db812
Elijah Guerrero <elijahguerrero97@gmail.com>
2020-04-29 22:36:31 -0500

    socket io handlers

---
+++ src/server.js
@@ -0,0 +1,39 @@
+const app = require('./app')
+const { PORT } = require('./config');
+const socket = require('socket.io');
+
+
+const server = app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`)
+});
+
+const io = socket(server);
+
+io.on('connection', socket => {
+  console.log('made socket connection: ', socket.id);
+  socket.emit('id', socket.id);
+
+  // io.emit('this', {will: 'be received by all'});
+
+
+
+  socket.on('message', (message) => {
+    console.log('received message: ',message);
+    //io.sockets.emit('message',message)
+
+    socket.broadcast.emit('message',message);
+  })
+
+  socket.on('typing', info => {
+    console.log("2 - RECEIVED FROM CLIENT");
+    console.log("2 - SENDING TO ALL SOCKETS THIS USER: "+ info.handle+ info.isTyping);
+    socket.broadcast.emit('typing', {'handle': info.handle, 'typing': info.isTyping});
+  })
+
+
+  socket.on('disconnect',() => {
+    io.emit('user disconnected');
+    console.log('disconnected');
+  });
+
+});
