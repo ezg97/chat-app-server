da1bb1c5c995dcf37a96c364a28a77f5881b86f7
Elijah Guerrero <elijahguerrero97@gmail.com>
2020-08-18 23:56:35 -0500

    added joining room functionality to server

--- src/server.js
+++ src/server.js
@@ -18,14 +18,38 @@ const server = app.listen(PORT, () => {
 });
 
 const io = socket(server);
-
+const users = [];
 io.on('connection', socket => {
   console.log('made socket connection: ', socket.id);
   socket.emit('id', socket.id);
+  //users.push(socket.id);
+  console.log('1', socket);
 
   // io.emit('this', {will: 'be received by all'});
+  console.log('2');
 
+  socket.on('addUser', (userID) => {
+      users.push({id: userID, sID: socket});
+      console.log('zmoney');
+      console.log({users});
+  });
+  console.log('3');
 
+  socket.on('JoinRoom', info => {
+    console.log("ROOM INFO: "+ info.room, info.targetId);
+    //socket.broadcast.emit('typing', {'handle': info.handle, 'typing': info.isTyping});
+    socket.join(info.room);
+    //search to see if the targetId (the targeted user we're trying to chat with) is in our users array (aka, is online)
+    const targetSocket = users.find(obj => obj.id === targetId);
+    //if not undefined then that means they were found in the array
+    if (targetSocket !== undefined) {
+      targetSocket.join(info.room);
+      socket.emit('connectionMade', info.targetId);
+    }
+    else {
+      socket.emit('connectionLost', info.targetId);
+    }
+  })
 
   socket.on('message', (message) => {
     console.log('received message: ',message);
@@ -43,7 +67,13 @@ io.on('connection', socket => {
 
   socket.on('disconnect',() => {
     io.emit('user disconnected');
+    users.find((obj, i) => {
+      if (obj.sID === socket.id) {
+        users.splice(i,1);
+      }
+    });
     console.log('disconnected');
+    console.log({users})
   });
 
 });
