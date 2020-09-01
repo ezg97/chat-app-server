aedebbbc10e1282af74957f13cefeddc21c9b804
Elijah Guerrero <elijahguerrero97@gmail.com>
2020-09-01 12:05:21 -0500

    fixed err

--- src/config/passport-setup.js
+++ src/config/passport-setup.js
@@ -192,7 +192,8 @@ passport.use(
         //console.log('made it to linkedIn account');
         console.log('user', profile);
         //check if user exists in database
-        UserService.hasUserWithEmail(db, `${profile.photos[0].value}${profile.displayName}`/*profile.id*/).then(currentUser => {
+        /*profile.id*/
+        UserService.hasUserWithEmail(db, `${profile.photos[0].value}${profile.displayName}`).then(currentUser => {
             //If the user is in the database then pass them into the callback function
             if (currentUser) {
                 console.log('USING USER');
@@ -206,7 +207,7 @@ passport.use(
                     user_name: profile.displayName,
                     user_id: profile.id,
                     user_thumbnail: profile.photos[0].value,
-                    user_email: `${profile.photos[0].value}${profile.displayName}`,/*profile.email8/ //combo of thumbnail and name to make unique identifer 
+                    user_email: `${profile.photos[0].value}${profile.displayName}`,/*profile.email*/ //combo of thumbnail and name to make unique identifer 
                 }
                 
                 // // Add user to database
