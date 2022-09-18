270566c06307632354a98b0d35665cfc4643eb15
Elijah Guerrero <40578753+ezg97@users.noreply.github.com>
2022-09-18 00:21:55 -0500

    updated postgrator-config with new key/value pairs

--- postgrator-config.js
+++ postgrator-config.js
@@ -6,8 +6,11 @@ module.exports = {
   "connectionString": (process.env.NODE_ENV === 'test')
     ? process.env.TEST_DATABASE_URL
     : process.env.DATABASE_URL,
-    ssl: {
-      SSL: !!process.env.SSL,
-      rejectUnauthorized: false
-    }
-}
+  ssl: true,
+    // SSL: !!process.env.SSL,
+  rejectUnauthorized: false,
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: true
+  },
+};
